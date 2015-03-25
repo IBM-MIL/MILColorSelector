798b2cd153109075dcbdeeeb0aca322dbec29b3a
Jim Avery <jlavery@us.ibm.com>
2015-03-25 09:33:13 -0500

    Changed name to color selector for better clarity

--- MILColorSelector/color-selector.js
+++ MILColorSelector/color-selector.js
@@ -4,8 +4,8 @@
  */
 
 /**
- *  @namespace MILColorPicker
- *  @description Defines the {@linkcode MILColorPicker} module, which includes the colorPicker directive.
+ *  @namespace MILColorSelector
+ *  @description Defines the {@linkcode MILColorSelector} module, which includes the colorSelector directive.
  *  @author Jim Avery
  *  @copyright © 2015 IBM Corporation. All Rights Reserved.
  */
@@ -15,26 +15,26 @@ var angular;
 (function () {
     'use strict';
 
-    var MILColorPicker = angular.module('MILColorPicker', []);
+    var MILColorSelector = angular.module('MILColorSelector', []);
 
     /**
-     *  @class MILColorPicker.colorPicker
-     *  @memberOf MILColorPicker
-     *  @description A directive that defines a color picker, which displays a variety of colors as selectable buttons. The directive
+     *  @class MILColorSelector.colorSelector
+     *  @memberOf MILColorSelector
+     *  @description A directive that defines a color selector, which displays a variety of colors as selectable buttons. The directive
      *  includes a hidden input, allowing for the current selection to be included in an HTML form. The name of the input is a directive
      *  attribute; the value is the "color" field of the current selection.
      *  This directive is decorated with four attributes: {@linkcode name} is the name of the hidden input where an HTML form can access
      *  the data from this directive. {@linkcode colorText} is the text that will be displayed before the currently selected color
-     *  (usually just "Color"). {@linkcode colorOptions} is an array of objects with color names and hex values. {@linkcode colorPicked}
+     *  (usually just "Color"). {@linkcode colorOptions} is an array of objects with color names and hex values. {@linkcode colorSelected}
      *  is an optional external function that will be triggered whenever a color is clicked; it should take in a single color object
      *  as a parameter.
      *  @example
-     *  <color-picker name="myColor" color-text="Color" color-options="[{color: 'Red', hexColor: '#F00'}]" color-picked="func(color)"></color-picker>
+     *  <color-selector name="myColor" color-text="Color" color-options="[{color: 'Red', hexColor: '#F00'}]" color-selected="func(color)"></color-selector>
      *  @author James Avery
      *  @copyright © 2015 IBM Corporation. All Rights Reserved.
      */
 
-    MILColorPicker.directive('colorPicker', function () {
+    MILColorSelector.directive('colorSelector', function () {
 
         return {
 
@@ -44,10 +44,10 @@ var angular;
                 name: '@',
                 colorText: '@',
                 colorOptions: '=',
-                colorPicked: '&'
+                colorSelected: '&'
             },
 
-            template: "<style>.color-pick-box {width: 25px;height: 25px;padding: 3px;border: 1px;border-style: solid;border-color: #888888;margin: 0px auto;background-color: white;z-index: 10;float: left;position: relative;}.color-pick-box-inner {content: '';display: block;border: 1px;border-style: solid;border-color: #DDD;position: absolute;top: 3px;bottom: 3px;left: 3px;right: 3px;z-index: -1;}</style><div ng-if='finalColors.length > 0'><input type='hidden' name='{{name}}' value='{{currentColor.color}}'><p class='color-picker-text' style='margin-bottom: 10px'>{{colorText}}: {{currentColor.color}}</p> <div ng-repeat-start='pick in finalColors' class='color-pick-box' ng-if='currentColor.hexColor == pick.hexColor' ng-click='changeColor($index)'><div class='color-pick-box-inner' style='background-color: {{pick.hexColor}}'></div></div><div ng-repeat-end class='color-pick-box' style='border-color: #ffffff;' ng-if='currentColor.hexColor != pick.hexColor' ng-click='changeColor($index)'><div class='color-pick-box-inner' style='background-color: {{pick.hexColor}}'></div></div></div>",
+            template: "<style>.color-select-box {width: 25px;height: 25px;padding: 3px;border: 1px;border-style: solid;border-color: #888888;margin: 0px auto;background-color: white;z-index: 10;float: left;position: relative;}.color-select-box-inner {content: '';display: block;border: 1px;border-style: solid;border-color: #DDD;position: absolute;top: 3px;bottom: 3px;left: 3px;right: 3px;z-index: -1;}</style><div ng-if='finalColors.length > 0'><input type='hidden' name='{{name}}' value='{{currentColor.color}}'><p class='color-selector-text' style='margin-bottom: 10px'>{{colorText}}: {{currentColor.color}}</p> <div ng-repeat-start='pick in finalColors' class='color-select-box' ng-if='currentColor.hexColor == pick.hexColor' ng-click='changeColor($index)'><div class='color-select-box-inner' style='background-color: {{pick.hexColor}}'></div></div><div ng-repeat-end class='color-select-box' style='border-color: #ffffff;' ng-if='currentColor.hexColor != pick.hexColor' ng-click='changeColor($index)'><div class='color-select-box-inner' style='background-color: {{pick.hexColor}}'></div></div></div>",
 
             link: function (scope, element, attrs) {
 
@@ -61,8 +61,8 @@ var angular;
                     scope.changeColor = function (colorIndex) {
                         scope.currentElem = colorIndex;
                         scope.currentColor = scope.colorOptions[scope.currentElem];
-                        if (scope.colorPicked !== null && scope.colorPicked !== undefined) {
-                            scope.colorPicked({
+                        if (scope.colorSelected !== null && scope.colorSelected !== undefined) {
+                            scope.colorSelected({
                                 color: scope.currentColor
                             });
                         }
@@ -79,10 +79,12 @@ var angular;
                         return retArray;
                     };
 
+                    console.log(String(scope.colorOptions.length));
                     /* Sanitize the provided color array before doing anything else. */
                     if (scope.colorOptions !== undefined && scope.colorOptions !== null && scope.colorOptions.constructor === Array && scope.colorOptions.length > 0) {
                         scope.finalColors = scope.cleanColors(scope.colorOptions);
                     }
+                    console.log(String(scope.finalColors.length));
 
                     /* Initialize a selected color. */
                     if (scope.finalColors.length > 0) {
--- README.md
+++ README.md
@@ -1,47 +1,47 @@
-MILColorPicker
+MILColorSelector
 ==============
 
 <p align="center">
-<img src="mil-color-picker.gif" alt="Animation" border=1 /></p>
+<img src="mil-color-selector.gif" alt="Animation" border=1 /></p>
 
-MILColorPicker is a reusable UI component written as an AngularJS directive, which defines a __color picker__ HTML element. The color picker creates a simple and intuitive visual interface, allowing users to easily view and select colors, and can be easily incorporated into a standard HTML form.
+MILColorSelector is a reusable UI component written as an AngularJS directive, which defines a __color selector__ HTML element. The color selector creates a simple and intuitive visual interface, allowing users to easily view and select colors, and can be easily incorporated into a standard HTML form.
 
 ## Installation
 
 Include the .js file in the `head` of the HTML file where the AngularJS controller is being applied.
 
 ```html
-<script src="[path to file]/color-picker.js"></script>
+<script src="[path to file]/color-selector.js"></script>
 ```
 
-Also include the `MILColorPicker` module as a dependency when defining your AngularJS module.
+Also include the `MILColorSelector` module as a dependency when defining your AngularJS module.
 
 ```js
-angular.module('myModule', ['MILColorPicker']);
+angular.module('myModule', ['MILColorSelector']);
 ```
 
 ## Usage
 
-The color picker can be inserted into an HTML document as in the example below:
+The color selector can be inserted into an HTML document as in the example below:
 
 ```html
-<color-picker name="myColor" color-text="Color" color-options="[{color: 'Red', hexColor: '#F00'},{color: 'Blue', hexColor: '#00F'}]" color-picked="func(color)" />
+<color-selector name="myColor" color-text="Color" color-options="[{color: 'Red', hexColor: '#F00'},{color: 'Blue', hexColor: '#00F'}]" color-selected="func(color)" />
 ```
 
-* `name` - This optional parameter attaches a name to the color picker so its data can be used in a standard HTML form. The associated `value` is the `color` element of whatever color has been chosen by the user. In the above example, the possible values are `Red` or `Blue`.
+* `name` - This optional parameter attaches a name to the color selector so its data can be used in a standard HTML form. The associated `value` is the `color` element of whatever color has been chosen by the user. In the above example, the possible values are `Red` or `Blue`.
 * `color-text` - This optional parameter indicates the text to appear in place of "Color" before the selected color's name is displayed. If no value is specified, "Color" will be used.
-* `color-options` - This parameter is an array of objects that represent the possible color options for the picker. Each object has two required fields, though additional fields can be included:
+* `color-options` - This parameter is an array of objects that represent the possible color options for the selector. Each object has two required fields, though additional fields can be included:
   * `color` - The name of the color. This will appear after "Color: " or whatever `color-text` is specified.
   * `hexColor` - The hex value of the color. This is the color that will appear inside the box corresponding to this color object. Note that each color is surrounded by a light-gray border, so near-white colors can be more easily seen.
   
-  If any of the color objects are invalid, they will not be displayed. If there are no valid color objects, the color picker will not display in any way.
-* `color-picked` - This optional parameter specifies an external function that will be executed whenever the user clicks on a color. Any such function should take in only one parameter, which is the color object corresponding to the selected color.
+  If any of the color objects are invalid, they will not be displayed. If there are no valid color objects, the color selector will not display in any way.
+* `color-selected` - This optional parameter specifies an external function that will be executed whenever the user clicks on a color. Any such function should take in only one parameter, which is the color object corresponding to the selected color.
 
-The color picker can be styled by making changes to the class `color-picker-text` for the text, or to `color-pick-box` for the color boxes. Note that if you wish to change the inner border color for a color box, the operative class is `color-pick-box-inner`.
+The color selector can be styled by making changes to the class `color-selector-text` for the text, or to `color-select-box` for the color boxes. Note that if you wish to change the inner border color for a color box, the operative class is `color-select-box-inner`.
 
 ## Requirements
 
-`MILColorPicker` is designed to work with AngularJS 1.3. It may work on older versions of AngularJS with little to no adjustment.
+`MILColorSelector` is designed to work with AngularJS 1.3. It may work on older versions of AngularJS with little to no adjustment.
 
 ## Author
 
@@ -49,4 +49,4 @@ Created by [Jim Avery](https://github.com/TheSoundDefense) at the [IBM Mobile In
 
 ## License
 
-`MILColorPicker` is available under the Apache 2.0 license. See the LICENSE file for more info.
+`MILColorSelector` is available under the Apache 2.0 license. See the LICENSE file for more info.
